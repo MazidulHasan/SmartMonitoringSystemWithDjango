4e2d2a63b2fac40b2aebc9207569cd519cdad6b9
MazidulHasan <md.mazidulhasan1@gmail.com>
2024-02-19 23:33:54 +0600

    Data pass from view to html page

--- home/views.py
+++ home/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "index.html")
+
+    peoples =[
+        {'name' : 'Name1', 'age': 10},
+        {'name' : 'Name2', 'age': 20},
+        {'name' : 'Name3', 'age': 30},
+        {'name' : 'Name4', 'age': 40},
+        {'name' : 'Name5', 'age': 50},
+        {'name' : 'Name6', 'age': 60},
+        {'name' : 'Name7', 'age': 70}
+    ]
+    return render(request, "index.html", context={'peoples':peoples})
 
 def success_page(request):
     return HttpResponse("Success page")
