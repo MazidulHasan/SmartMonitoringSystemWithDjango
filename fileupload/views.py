56887f98a1833d87a0aa2a1e2f49a10870d7be23
MazidulHasan <md.mazidulhasan1@gmail.com>
2024-02-22 00:25:08 +0600

    Delete fix: Will delete from storage also

--- fileupload/views.py
+++ fileupload/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from .models import *
 import os
 from django.conf import settings
-
+from pathlib import Path
 
 def uploadfiles(request):
     if request.method == "POST":
@@ -34,19 +34,26 @@ def uploadedFileList(request):
     context = {'uploadedFiles':queryset}
     return render(request,'files/fileList.html', context)
 
-def delete_file(id):
+def delete_file(request,id):
     queryset = fileupload.objects.get(id=id)
-    # file_name = queryset.get(file_name)
-    # print(file_name)
+    file_name = os.path.normpath(queryset.file_name.name)  # Normalize the file path
+    print("File name is ::::::::::::::::::::::::::::::::::::")
+    print(file_name)
     queryset.delete()
-    # file_path = os.path.join(BASE_DIR,'/media/'+file_name)
-    # print("File paths are..............")
-    # print(BASE_DIR)
-    # print(file_path)
-    # if os.path.exists(file_path):
-    #     # Delete the file
-    #     os.remove(file_path)
-    #     print("File deleted")
-    # else:
-    #     print("File not found")
+    print("settings.BASE_DIR")
+    print(settings.BASE_DIR)
+
+    # Additional directories to append to the file path
+    additional_directories = ['public', 'static',]
+
+    # Construct the full file path by joining all directories
+    file_path = os.path.join(settings.BASE_DIR, *additional_directories, file_name)
+
+    print(file_path)
+    if os.path.exists(file_path):
+        # Delete the file
+        os.remove(file_path)
+        print("File deleted")
+    else:
+        print("File not found")
     return redirect('/uploadedFileList/')
